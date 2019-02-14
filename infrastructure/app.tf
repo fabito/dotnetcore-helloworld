3a99756ab96a1e262d094a8231c037fc4ef7ec03
fabito <fuechi@ciandt.com>
2019-02-14 01:25:36 -0200

    moving to app service

---
+++ infrastructure/app.tf
@@ -0,0 +1,57 @@
+# resource "random_id" "server" {
+#   keepers = {
+#     azi_id = 1
+#   }
+#   byte_length = 8
+# }
+resource "azurerm_resource_group" "test" {
+  name     = "${var.resource_group_name}"
+  location = "${var.location}"
+}
+resource "azurerm_app_service_plan" "test" {
+  name                = "${var.service_plan_name}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  kind                = "Linux"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  properties {
+    reserved = true
+  }
+}
+resource "azurerm_app_service" "test" {
+  name                = "${var.app_service_name}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+	always_on = true
+    linux_fx_version = "DOCKER|(${var.app_service_docker_image})"
+  }
+
+  app_settings {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+}
+resource "azurerm_app_service_slot" "test" {
+  name                = "${var.app_service_slot_name}"
+  app_service_name    = "${azurerm_app_service.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+	always_on = true
+    linux_fx_version = "DOCKER|(${var.app_service_docker_image})"
+  }
+
+  app_settings {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,27 +1,7 @@
-output "client_key" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_key}"
+output "default_site_hostname " {
+    value = "${azurerm_app_service.test.default_site_hostname}"
 }
 
-output "client_certificate" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate}"
-}
-
-output "cluster_ca_certificate" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate}"
-}
-
-output "cluster_username" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
-}
-
-output "cluster_password" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
-}
-
-output "kube_config" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config_raw}"
-}
-
-output "host" {
-    value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+output "staging_site_hostname " {
+    value = "${azurerm_app_service_slot.test.default_site_hostname}"
 }
